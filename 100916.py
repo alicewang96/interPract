af3b43499fa3b2c2747d74be5c125a607f9a6a67
vicky11z <vicky11z@berkeley.edu>
2016-10-09 21:14:03 -0700

    added comments for sum left leaves function

--- 100916.py
+++ 100916.py
@@ -12,13 +12,19 @@ def sum_left_leaves(bn, left_sum):
 	if bn == None:
 		return 0
 	if bn.left == None and bn.right == None:
+		# at a leaf node - return the sum we've created so far
 		return left_sum
 	tot_sum = 0
 	if bn.left:
+		# there exists a node on the left side
 		if bn.left.left == None and bn.left.right == None:
+			# bn.left is a left leaf node
 			tot_sum += bn.left.val
 		else:
+			# bn.left is not a left leaf node, want to sum the left leaves in the
+			# tree that has bn.left as its root
 			tot_sum += sum_left_leaves(bn.left, left_sum)
+	# need to add left leaves of the right tree
 	tot_sum += sum_left_leaves(bn.right, left_sum)
 
 	return tot_sum
